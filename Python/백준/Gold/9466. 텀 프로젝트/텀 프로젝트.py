03c9582da64df37be8f71483e2dd06de847428ef
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-01-12 23:20:11 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1768 ms, Memory: 48448 KB -BaekjoonHub

--- Python/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ Python/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -8,36 +8,24 @@ DFS
 바로 떠오르는 풀이가 시간초과, 메모리초과 연달아 뜨는걸 보니 기초부터 잘못됐다는 생각이 들었다.
 '''
 import sys; input = sys.stdin.readline
-sys.setrecursionlimit(111111)
 
-
-def solve():
-    def dfs(now, depth=1):
-        nonlocal ans
-
-        visit[now] = True
-        cycle[now] = depth
-
-        nxt = arr[now]
-        
-        depth += 1
-        if visit[nxt]:
-            if nxt in cycle:
-                ans -= depth - cycle[nxt]
-            return
-
-        dfs(nxt, depth)
-        
-    N = int(input())
-    arr = [*map(lambda x: int(x)-1, input().split())]
-    visit = [False] * N
-    ans = N
-    for i in range(N):
-        if visit[i]: continue
-        cycle = dict()
-        dfs(i)
-
-    print(ans)
 
 for _ in range(int(input())):
-    solve()
+    N = int(input())
+
+    arr = [*map(lambda x: int(x)-1, input().split())]
+    
+    child = [0]*N
+    
+    for i in arr:
+        child[i] += 1
+    leaves = [i for i in range(N) if child[i] == 0]
+
+    for leaf in leaves:
+        idx = arr[leaf]
+
+        child[idx] -= 1
+        
+        if child[idx] == 0:
+            leaves.append(idx)
+    print(len(leaves))
