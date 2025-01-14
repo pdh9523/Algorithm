2a0439a39df0e00fc65fc7f50f59dfcb3f9bc1fc
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-01-14 15:43:17 +0900

    [Silver IV] Title: 찐 Even Number, Time: 644 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/32981. 찐 Even Number/찐 Even Number.py
+++ Python/백준/Silver/32981. 찐 Even Number/찐 Even Number.py
@@ -1,26 +1,13 @@
 import sys; input = sys.stdin.readline
 
 
-def memo(f):
-    cache = dict()
-    def wrapper(*args):
-        if args not in cache:
-            cache[args] = f(*args) % MOD
-        return cache[args]
-    return wrapper
-
-@memo
 def div_conq(num):
     if num == 0: return 1
     if num == 1: return 5
 
     if num % 2:
-        half = div_conq(num//2)
-        odd = div_conq(num//2+1)
-        return half * odd
-
-    half = div_conq(num//2)
-    return half * half
+        return div_conq(num//2)**2 * 5 % MOD
+    return div_conq(num//2) ** 2 % MOD
 
 
 MOD = 10**9+7
