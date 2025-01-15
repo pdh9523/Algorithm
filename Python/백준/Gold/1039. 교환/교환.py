b708d7b973942d30ed503242e052975497627228
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-01-15 18:20:17 +0900

    [Gold II] Title: 교환, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1039. 교환/교환.py
+++ Python/백준/Gold/1039. 교환/교환.py
@@ -1,35 +1,30 @@
-from collections import deque
+'''
+교환
 
+탐색
 
-def bfs():
-    new_q = set()
-    while q:
-        now = list(q.popleft())
-        
+그냥 자리수 교환하는 문제
+'''
+def search():
+    next_stack = set()
+    while stack:
+        now = list(stack.pop())
         for i in range(length-1):
             for j in range(i+1, length):
                 now[i],now[j] = now[j],now[i]
-
-                if now[0] == "0": 
-                    now[i],now[j] = now[j],now[i]
-                    continue
-                
-                new_q.add(tuple(now))
+                if now[0] != "0": next_stack.add(tuple(now))
                 now[i],now[j] = now[j],now[i]
-    return deque(new_q)
+    return list(next_stack)
 
 def join(arr):
     return "".join(arr)
 
 N,K = input().split()
 length = len(N)
-K = int(K)
-q = deque([list(N)])
+stack = [tuple(N)]
 
-for _ in range(K):
-    q = bfs()
+for _ in range(int(K)):
+    if not stack: break
+    stack = search()
 
-if not q:
-    print(-1)
-else:   
-    print(max([*map(join,q)]))
+print(-1 if not stack else max(map(join,stack)))
