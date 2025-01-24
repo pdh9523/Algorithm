8e41fa8adef4b4cea6ad03af7b7d66d177c6a5b3
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-01-24 21:08:37 +0900

    [Bronze II] Title: 수 정렬하기, Time: 4 ms, Memory: 852 KB -BaekjoonHub

---
+++ Go/백준/Bronze/2750. 수 정렬하기/수 정렬하기.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var (
+	br = bufio.NewReader(os.Stdin)
+	bw = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	line, _, _ := br.ReadLine()
+	N, _ := strconv.Atoi(string(line))
+
+	arr := make([]int, N)
+	for i := 0; i < N; i++ {
+		line, _, _ := br.ReadLine()
+		arr[i], _ = strconv.Atoi(string(line))
+	}
+
+	sort.Ints(arr)
+
+	defer bw.Flush()
+	for i := range arr {
+		bw.WriteString(strconv.Itoa(arr[i]))
+		bw.WriteString("\n")
+	}
+}
