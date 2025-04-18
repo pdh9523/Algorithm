df2d5cc00f8789d80251bc95fce70335b4d0654d
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-04-18 09:16:57 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14036 KB -BaekjoonHub

--- Java/백준/Gold/2631. 줄세우기/줄세우기.java
+++ Java/백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class Main {
@@ -9,30 +10,37 @@ public class Main {
 
          int N = Integer.parseInt(br.readLine());
          int[] arr = new int[N];
-         int[] DP = new int[N];
-
          for (int i=0; i<N; i++) {
              arr[i] = Integer.parseInt(br.readLine());
-             DP[i] = 1;
          }
 
-        for (int i=1; i<N; i++) {
-            for (int j=0; j<i; j++) {
-                if (arr[j] < arr[i]) {
-                    DP[i] = Math.max(DP[i], DP[j] + 1);
-                }
-            }
-        }
-        System.out.println(N-max(DP));
+         ArrayList<Integer> DP = new ArrayList<>();
+         for (int num: arr) {
+             if (DP.isEmpty() || DP.get(DP.size() -1) < num) {
+                 DP.add(num);
+             } else {
+                 DP.set(binarySearch(DP, num), num);
+             }
+         }
+
+        System.out.println(N - DP.size());
+
     }
 
-    static int max(int[] arr) {
-        int res = 0;
-        for (int i=0; i<arr.length; i++) {
-            if (res < arr[i]) {
-                res = arr[i];
+    static int binarySearch(ArrayList<Integer> arr, int target) {
+        int left = 0, right = arr.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr.get(mid) == target) {
+                return mid;
+            }
+
+            if (arr.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return res;
+        return left;
     }
 }
