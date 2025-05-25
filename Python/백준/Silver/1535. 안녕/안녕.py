78047140b90b6ba37273d4564fb91e4c6f581ab1
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-05-25 20:41:12 +0900

    [Silver II] Title: 안녕, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1535. 안녕/안녕.py
@@ -0,0 +1,16 @@
+import sys; input = sys.stdin.readline
+
+
+N = int(input())
+arr = [a for a in zip(map(int,input().split()), map(int,input().split()))]
+
+DP = [-1] * 100
+DP[0] = 0
+
+for i,j in arr:
+    for k in range(99, -1, -1):
+        if DP[k] == -1: continue
+        if k+i >= 100: continue
+        DP[k+i] = max(DP[k+i], DP[k]+j)
+
+print(max(DP))
