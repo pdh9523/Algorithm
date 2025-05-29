d36376b54da967d7b397902dc709894ddfadadc6
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-05-30 00:18:05 +0900

    [Platinum III] Title: 별다줄, Time: 908 ms, Memory: 455892 KB -BaekjoonHub

---
+++ Go/백준/Platinum/17365. 별다줄/별다줄.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+type Node struct {
+	children []*Node
+	cnt      int64
+}
+
+func Ctoi(c rune) int {
+	return int(c - 'a')
+}
+
+type Trie struct {
+	root *Node
+}
+
+func (t *Trie) Insert(s string) {
+	runes := []rune(s)
+
+	now := t.root
+	for _, char := range runes {
+		idx := Ctoi(char)
+		if now.children[idx] == nil {
+			now.children[idx] = &Node{make([]*Node, 26), 0}
+		}
+		now = now.children[idx]
+		now.cnt++
+	}
+}
+
+func newTrie() *Trie {
+	return &Trie{&Node{make([]*Node, 26), 0}}
+}
+
+const (
+	MOD  = 1000000007
+	size = 26
+)
+
+func main() {
+	bs.Buffer(buf, 1024*1024)
+	bs.Split(bufio.ScanWords)
+
+	defer bw.Flush()
+
+	trie := newTrie()
+	N := intInput()
+	for i := 0; i < N; i++ {
+		trie.Insert(input())
+	}
+
+	word := input()
+	length := len(word)
+
+	DP := make([]int64, length+1)
+	DP[0] = 1
+	for i := 0; i < length; i++ {
+		now := trie.root
+		for j := i; j < length; j++ {
+			idx := Ctoi(rune(word[j]))
+			if now.children[idx] == nil {
+				break
+			}
+			now = now.children[idx]
+			DP[j+1] = (DP[i]*now.cnt + DP[j+1]) % MOD
+		}
+	}
+	bw.WriteString(strconv.Itoa(int(DP[length])))
+}
+
+var (
+	bs  = bufio.NewScanner(os.Stdin)
+	bw  = bufio.NewWriter(os.Stdout)
+	buf = make([]byte, 1024*1024)
+)
+
+func input() string {
+	bs.Scan()
+	return bs.Text()
+}
+
+func intInput() int {
+	num, _ := strconv.Atoi(input())
+	return num
+}
