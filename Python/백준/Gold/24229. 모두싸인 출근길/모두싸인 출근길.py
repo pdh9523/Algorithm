ea3d73b94bce2e20ffdb30b13bfafb450ce418a5
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-06-05 19:06:01 +0900

    [Gold IV] Title: 모두싸인 출근길, Time: 432 ms, Memory: 61944 KB -BaekjoonHub

--- Python/백준/Gold/24229. 모두싸인 출근길/모두싸인 출근길.py
+++ Python/백준/Gold/24229. 모두싸인 출근길/모두싸인 출근길.py
@@ -2,13 +2,13 @@ import sys; input = sys.stdin.readline
 
 
 N = int(input())
-arr = sorted([tuple(map(int,input().split())) for _ in range(N)])
+arr = iter(sorted([tuple(map(int,input().split())) for _ in range(N)]))
 
-start,end = arr[0]
+start,end = next(arr)
 res = []
 jump = end-start
 reach = end + jump
-for s,e in arr[1:]:
+for s,e in arr:
     if s <= end:
         end = max(end, e)
     else:
