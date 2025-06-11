22db3de8c03069dd9850df866ff73512babd2971
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-06-11 12:02:02 +0900

    [Platinum V] Title: 임계경로, Time: 216 ms, Memory: 50588 KB -BaekjoonHub

---
+++ Python/백준/Platinum/1948. 임계경로/임계경로.py
@@ -0,0 +1,46 @@
+import sys; input = sys.stdin.readline
+from collections import deque
+
+
+N = int(input())
+M = int(input())
+
+in_degree = [0] * (N+1)
+graph = [[] for _ in range(N+1)]
+reversed_graph = [[] for _ in range(N+1)]
+cnt = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+    reversed_graph[b].append(a)
+    in_degree[b] += 1
+start, end = map(int,input().split())
+
+q = deque([start])
+visit = [0] * (N+1)
+while q:
+    now = q.popleft()
+    for nxt,cost in graph[now]:
+        in_degree[nxt] -= 1
+    
+        if visit[nxt] < visit[now] + cost:
+            visit[nxt] = visit[now] + cost
+            cnt[nxt] = [now]
+        elif visit[nxt] == visit[now] + cost:
+            cnt[nxt].append(now)
+        
+        if in_degree[nxt] == 0:
+            q.append(nxt)
+
+q = deque([end])
+route = set()
+while q:
+    now = q.popleft()
+    for x in cnt[now]:
+        if (now,x) not in route:
+            route.add((now,x))
+            q.append(x)
+
+print(visit[end])
+print(len(route))
