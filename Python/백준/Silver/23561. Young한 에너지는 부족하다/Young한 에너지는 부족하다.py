a79656a0e1608397df7e2e7d9c21d857735e1574
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-07-15 22:12:23 +0900

    [Silver II] Title: Young한 에너지는 부족하다, Time: 184 ms, Memory: 66292 KB -BaekjoonHub

--- Python/백준/Silver/23561. Young한 에너지는 부족하다/Young한 에너지는 부족하다.py
+++ Python/백준/Silver/23561. Young한 에너지는 부족하다/Young한 에너지는 부족하다.py
@@ -1,11 +1,4 @@
 N = int(input())
-if N == 1: exit(print(0))
-
 arr = sorted(map(int,input().split()))
-for _ in range(N): arr.pop()
 
-max_v = arr.pop()
-for _ in range(N-1):
-    min_v = arr.pop()
-
-print(max_v-min_v)
+print(arr[2*N-1]-arr[N])
