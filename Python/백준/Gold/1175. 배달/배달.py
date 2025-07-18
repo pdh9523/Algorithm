e98c740c56d0857099b2ba446d7ea429d88dd69c
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-07-19 01:23:10 +0900

    [Gold I] Title: 배달, Time: 68 ms, Memory: 35004 KB -BaekjoonHub

--- Python/백준/Gold/1175. 배달/배달.py
+++ Python/백준/Gold/1175. 배달/배달.py
@@ -1,61 +1,33 @@
-'''
-배달
-
-BFS, 비트마스킹 
-
-해당 좌표에 들어온 방향으로 다시 들어오는 것은 의미 없는 행동이니 비트마스킹을 통해 컷한다.
-'''
-import sys; input = lambda: sys.stdin.readline().rstrip()
+import sys; input = lambda: sys.stdin.readline().strip()
 from collections import deque
 
 
-def multi_bfs(i,j):
-    visit = [[-1]*M for _ in range(N)]
+def bfs(i,j,d=-1):
+    visit = [[0]*M for _ in range(N)]
     dr = [[0]*M for _ in range(N)]
-    visit[i][j] = 0
-    
-    q = deque([(i,j,-1)])
-    answer = []
+
+    visit[i][j] = 1
+    q = deque([(i,j,d)])
+
     while q:
         x,y,d = q.popleft()
 
         if arr[x][y] == "C":
-            dist_nxt = bfs(x,y,d)
-            if dist_nxt:
-                answer.append(visit[x][y] + dist_nxt)
-
-        for i in range(4):
-            if i==d: continue
-            nx,ny = x+dx[i], y+dy[i]
-            if 0 <= nx < N and 0 <= ny < M:
-                if dr[nx][ny] & 1<<i: continue
-                if arr[nx][ny] == "#": continue
-                dr[nx][ny] |= 1<<i
-                visit[nx][ny] = visit[x][y] + 1
-                q.append((nx,ny,i))
-    return answer
-
-def bfs(ci, cj, d):
-    visit = [[-1]*M for _ in range(N)]
-    dr = [[0]*M for _ in range(N)]
-    visit[ci][cj] = 0
-    
-    q = deque([(ci,cj,d)])
-    while q:
-        x,y,d = q.popleft()
-        if not(x == ci and y == cj) and arr[x][y] == "C":
-            return visit[x][y]
+            arr[x][y] = "S"
+            return visit[x][y]-1, d
 
         for i in range(4):
             if d == i: continue
+
             nx, ny = x+dx[i], y+dy[i]
+
             if 0 <= nx < N and 0 <= ny < M:
                 if dr[nx][ny] & 1<<i: continue
                 if arr[nx][ny] == "#": continue
-                dr[nx][ny] |= 1<<i
+                dr[nx][ny] += 1<<i
                 visit[nx][ny] = visit[x][y]+1
                 q.append((nx,ny,i))
-    return 0
+    return -1,-1
 
 def find(char):
     for i in range(N):
@@ -70,5 +42,12 @@ dy = 0, 1, 0, -1
 N,M = map(int,input().split())
 arr = [list(input()) for _ in range(N)]
 
-answer = multi_bfs(*find("S"))
-print(-1 if not answer else min(answer))
+ans = 0
+d = -1
+for _ in range(2):
+    tmp, d = bfs(*find("S"), d)
+    if tmp == -1: 
+        ans = -1
+        break
+    ans += tmp 
+print(ans)
