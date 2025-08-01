0f0bfc6bd09427b9733ebd6fb73a8d23ba56f118
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-08-01 16:11:03 +0900

    [Gold V] Title: 전구 상태 뒤집기, Time: 220 ms, Memory: 67024 KB -BaekjoonHub

---
+++ Python/백준/Gold/25634. 전구 상태 뒤집기/전구 상태 뒤집기.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = [x for x in zip(map(int,input().split()), map(int,input().split()))]
+
+on,off,max_v = 0,0,-float('inf')
+for i in range(N):
+    x, is_on = arr[i]
+
+    if is_on:
+        on += x
+        off -= x
+        max_v = max(max_v, -1*x)
+    else:
+        off += x
+        off = max(off, x)
+        max_v = max(max_v, off)
+
+print(on+max_v)
