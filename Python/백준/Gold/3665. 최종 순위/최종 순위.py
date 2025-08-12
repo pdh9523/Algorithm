e11ff20591c485fe8f534970cd84b29761fca149
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-08-12 22:30:20 +0900

    [Gold I] Title: 최종 순위, Time: 412 ms, Memory: 42792 KB -BaekjoonHub

--- Python/백준/Gold/3665. 최종 순위/최종 순위.py
+++ Python/백준/Gold/3665. 최종 순위/최종 순위.py
@@ -15,16 +15,12 @@ def solve():
     
     for _ in range(int(input())):
         a,b = map(int,input().split())
-        if b in graph[a]:
-            graph[a].discard(b)
-            in_degree[b] -= 1
-            graph[b].add(a)
-            in_degree[a] += 1
-        else:
-            graph[b].discard(a)
-            in_degree[a] -= 1
-            graph[a].add(b)
-            in_degree[b] += 1
+        if a in graph[b]: a,b = b,a
+        
+        graph[a].discard(b)
+        in_degree[b] -= 1
+        graph[b].add(a)
+        in_degree[a] += 1
     ans = []
     q = deque([x for x in range(1,N+1) if in_degree[x] == 0])
     while q:
@@ -36,9 +32,7 @@ def solve():
             if in_degree[nxt] == 0:
                 q.append(nxt)
     
-    if len(ans) != N:
-        return print("IMPOSSIBLE")
-    else:
-        return print(" ".join(map(str, ans)))
+    if len(ans) != N: return print("IMPOSSIBLE")
+    else: return print(*ans)
 
 for _ in range(int(input())): solve()
