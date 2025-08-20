b16379a26d005ad64075a0f7a41ccc818ab17d42
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-08-20 23:24:37 +0900

    [Platinum V] Title: 안수빈수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Platinum/16680. 안수빈수/안수빈수.py
+++ Python/백준/Platinum/16680. 안수빈수/안수빈수.py
@@ -1,17 +1,2 @@
 import sys; input = sys.stdin.readline
-
-
-def calc_digit(num):
-    res = 0 
-    while num:
-        res += num%10
-        num //=10
-    return res
-
-def check(num):
-    while num <= 10**18:
-        if calc_digit(num) % 2: return num
-        num *= 2
-
-for _ in range(int(input())):
-    print(check(int(input())))
+for _ in range(int(input())): print(int(input())*(10**9-1))
