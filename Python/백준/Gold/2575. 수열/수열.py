93ab52a881e3f05e4e569c3e70d3cb42fc97040b
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-08-27 22:54:21 +0900

    [Gold IV] Title: 수열, Time: 112 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Gold/2575. 수열/수열.py
+++ Python/백준/Gold/2575. 수열/수열.py
@@ -1,13 +1,5 @@
-# A 문제: 3으로 나눠보기 (왠지는 모르겠는데, 3이 제일 가성비 좋은 것 같음)
-# 10 -> 3 3 2 2 // 11 -> 3 3 3 2  // 12 -> 3 3 3 3
-# B 문제: 소인수분해 하면서 개수 더하기
 def get_A(num):
-    case = num % 3
-    if case == 1:
-        return num // 3 + 1
-    if case == 2:
-        return num // 3 + 1
-    return num // 3 
+    return (num-1) // 3 + 1
 
 def get_B(num):
     if num == 1: return 1
@@ -19,9 +11,7 @@ def get_B(num):
             res += 1
         else:
             div += 1
-    if num == 2:
-        res += 1
-    return res
+    return res + (num == 2)
 
 N = int(input())
 
