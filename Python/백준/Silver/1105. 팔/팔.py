2a1982799c38ba3a2960b50e2fdcf68b480b23da
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-08-29 10:35:49 +0900

    [Silver I] Title: 팔, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1105. 팔/팔.py
+++ Python/백준/Silver/1105. 팔/팔.py
@@ -1,10 +1,8 @@
-L,R = input().split()
-length = max(len(L),len(R))
-L,R = L.zfill(length), R.zfill(length)
+length = 10
+L,R = map(lambda x: x.zfill(length), input().split())
 
 cnt = 0
 for l,r in zip(L,R):
-    if l==r=="8":
-        cnt += 1
+    if l==r=="8": cnt += 1
     elif l!=r: break
 print(cnt)
