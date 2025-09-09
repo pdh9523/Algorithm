76a38c1e006c572eb7b516667409664bc1de1c6b
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-09-09 21:05:58 +0900

    [Silver II] Title: 약속, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1183. 약속/약속.py
+++ Python/백준/Silver/1183. 약속/약속.py
@@ -1,8 +1,6 @@
 import sys; input = sys.stdin.readline
 
 
-def diff(a,b): return a-b
-
 N = int(input())
-arr = sorted([diff(*map(int,input().split())) for _ in range(N)])
+arr = sorted([(lambda a,b: a-b)(*map(int,input().split())) for _ in range(N)])
 print(1 if N%2 else abs(arr[N//2]-arr[N//2-1]+1))
