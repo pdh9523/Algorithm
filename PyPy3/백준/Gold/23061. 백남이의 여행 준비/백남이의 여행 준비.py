87ff1bc6b4f296304a71b371d7ab8de1116d0375
박동현 <156387954+pdh9523@users.noreply.github.com>
2025-09-12 14:43:04 +0900

    [Gold III] Title: 백남이의 여행 준비, Time: 668 ms, Memory: 117360 KB -BaekjoonHub

--- PyPy3/백준/Gold/23061. 백남이의 여행 준비/백남이의 여행 준비.py
+++ PyPy3/백준/Gold/23061. 백남이의 여행 준비/백남이의 여행 준비.py
@@ -2,7 +2,7 @@ import sys; input = sys.stdin.readline
 
 
 N,M = map(int,input().split())
-arr = sorted([[*map(int,input().split())] for _ in range(N)], key=lambda x: -x[1])
+arr = [[*map(int,input().split())] for _ in range(N)]
 bags = [int(input()) for _ in range(M)]
 max_size = max(bags)
 
@@ -11,9 +11,9 @@ for w,v in arr:
     for i in range(max_size, w-1, -1):
         DP[i] = max(DP[i], DP[i-w]+v)
 
-for i in range(1,max_size+1):
-    if not DP[i] or DP[i-1] > DP[i]:
-        DP[i] = DP[i-1]
+# for i in range(1,max_size+1):
+#     if not DP[i] or DP[i-1] > DP[i]:
+#         DP[i] = DP[i-1]
 
 max_v = -1
 ans = -1
