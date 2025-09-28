f3e27ffd684611275f570b7fa1fc6c80714a50a0
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-09-29 08:09:04 +0900

    [Gold IV] Title: 우체국, Time: 292 ms, Memory: 53432 KB -BaekjoonHub

---
+++ Python/백준/Gold/2285. 우체국/우체국.py
@@ -0,0 +1,8 @@
+import sys; input = sys.stdin.readline
+
+
+arr = sorted([[*map(int,input().split())] for _ in range(int(input()))])
+s,d = sum(map(lambda x: x[1], arr))+1,0
+for a,b in arr:
+    d += b
+    if d >= s//2: exit(print(a))
