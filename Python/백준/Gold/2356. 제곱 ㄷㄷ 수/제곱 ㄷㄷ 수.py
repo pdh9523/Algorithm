1a5e45bd3761fb492764596e8bbda62373eda358
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-10-04 04:01:36 +0900

    [Gold II] Title: 제곱 ㄷㄷ 수, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

--- Python/백준/Gold/2356. 제곱 ㄷㄷ 수/제곱 ㄷㄷ 수.py
+++ Python/백준/Gold/2356. 제곱 ㄷㄷ 수/제곱 ㄷㄷ 수.py
@@ -1,21 +1,14 @@
-from math import sqrt
+from math import isqrt
 
 
 def is_square(num):
-    sq = int(sqrt(num))
-    return sq ** 2 == num
+    return isqrt(num) ** 2 == num
 
 def convert(char):
     for i in range(len(char)):
         for j in range(i+1,len(char)+1):
             if is_square(int(char[i:j])):
-                return (str(int(char) // (10**(len(char)-j)) +1) + "2"*(len(char)-j))
+                return convert((str(int(char) // (10**(len(char)-j))+1) + "2"*(len(char)-j)))
     return char
 
-N = input()
-while True:
-    if N != (tmp:=convert(N)):
-        N = tmp
-    else: break
-
-print(N)
+print(convert(input()))
