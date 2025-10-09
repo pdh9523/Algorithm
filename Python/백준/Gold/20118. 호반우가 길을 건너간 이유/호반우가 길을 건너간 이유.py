f770c5bd75e8208e485d8c042126db38a9f5855c
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-10-10 08:38:42 +0900

    [Gold II] Title: 호반우가 길을 건너간 이유, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Gold/20118. 호반우가 길을 건너간 이유/호반우가 길을 건너간 이유.py
+++ Python/백준/Gold/20118. 호반우가 길을 건너간 이유/호반우가 길을 건너간 이유.py
@@ -1,21 +1,13 @@
-N,M = map(int,input().split())
+N,M=map(int,input().split())
 
-tmp = []
-for i in range(N):
-    tmp.append((i,0))
+a=[]
+for i in range(N): a.append((i,0))
+for i in range(1,M-1): a.append((N-1,i))
+if len(a)%2==0: a.append((N-2, M-2))
+a.append((N-1,M-1))
 
-for i in range(1,M-1):
-    tmp.append((N-1,i))
-
-if len(tmp)%2==0:
-    tmp.append((N-2, M-2))
-
-tmp.append((N-1,M-1))
-
-ans = []
-for i in range(0, len(tmp), 2):
-    ans.extend([tmp[i], tmp[i+1]] * 2)
-
-print(len(ans))
-for a in ans:
-    print(*a)
+print(len(a)*2)
+for i in range(0,len(a),2):
+    for _ in range(2):
+        print(*a[i])
+        print(*a[i+1])
