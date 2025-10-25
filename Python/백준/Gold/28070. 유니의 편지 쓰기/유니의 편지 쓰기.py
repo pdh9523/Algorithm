f76ab0c122aa7ef87c223e705e79b2f66b19ba66
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-10-25 23:39:54 +0900

    [Gold V] Title: 유니의 편지 쓰기, Time: 356 ms, Memory: 57308 KB -BaekjoonHub

--- Python/백준/Gold/28070. 유니의 편지 쓰기/유니의 편지 쓰기.py
+++ Python/백준/Gold/28070. 유니의 편지 쓰기/유니의 편지 쓰기.py
@@ -1,29 +1,20 @@
 import sys; input = sys.stdin.readline
 
 
-def str_to_int(month: str) -> int:
-    year, month = month.split("-")
-    return int(year)*100 + int(month)
-
-def int_to_str(month: int) -> str:
-    year,month = divmod(month, 100)
-    return f"{year}-{month:02}"
-
 arr = [] 
 for _ in range(int(input())):
-    s,e = map(str_to_int, input().split())
+    s,e = input().split()
     arr.append((s,-1))
     arr.append((e,1))
-
 arr.sort()
 
 now = 0
 max_v = 0
 ans = ""
-for yyyymm, i in arr:
+for d,i in arr:
     now -= i
     if now > max_v:
-        ans = yyyymm
+        ans = d
         max_v = now
 
-print(int_to_str(ans))
+print(ans)
