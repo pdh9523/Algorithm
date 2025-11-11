bf01a34aa4b89a909548ab6a1a2a9af81f163f8c
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-11-11 09:21:12 +0900

    [Gold I] Title: 뭉쳐야 산다, Time: 1944 ms, Memory: 190088 KB -BaekjoonHub

---
+++ Python/백준/Gold/28277. 뭉쳐야 산다/뭉쳐야 산다.py
@@ -0,0 +1,30 @@
+import sys; input = sys.stdin.readline
+
+
+def get_set(it):
+    _, *x = it
+    return set(x)
+
+def stl(a,b):
+    if len(sets[a]) < len(sets[b]):
+        sets[a], sets[b] = sets[b], sets[a]
+    sets[a].update(sets[b])
+    sets[b].clear()
+
+def sout(x):
+    print(len(sets[x]))
+
+N,Q = map(int,input().split())
+
+sets = [set() for _ in range(N+1)]
+for i in range(1, N+1):
+    sets[i] = get_set(map(int,input().split()))
+
+cmd = {
+    1: stl,
+    2: sout
+}
+
+for _ in range(Q):
+    a, *q = map(int,input().split())
+    cmd[a](*q)
