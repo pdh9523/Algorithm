d0f9668d7d27947b7d8f00dc016900b9b2e414f7
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-11-25 01:25:04 +0900

    [Gold II] Title: 꽤 좋은 수, Time: 164 ms, Memory: 118392 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/9213. 꽤 좋은 수/꽤 좋은 수.py
@@ -0,0 +1,24 @@
+import sys; input = sys.stdin.readline
+
+
+SIZE = 1000000
+
+def is_good(num, diff):
+    return abs(num-data[num]) <= diff
+
+data = [1] * (SIZE+1)
+
+for s in range(2, SIZE//2+1):
+    for t in range(s*2, SIZE+1, s):
+        data[t] += s
+
+tc = 0
+while True:
+    start, end, diff = map(int,input().split())
+    if start == 0: break
+    
+    ans = 0
+    for x in range(start, end+1):
+        ans += is_good(x, diff)
+    
+    print(f"Test {(tc:=tc+1)}: {ans}")
