5af4c83bd691cb7465c2f0e9c17319983019b462
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2025-12-11 09:37:06 +0900

    [Gold V] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Easy), Time: 12 ms, Memory: 980 KB -BaekjoonHub

--- Go/백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.go
+++ Go/백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.go
@@ -3,52 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
+	"math/bits"
 	"os"
 	"strconv"
 )
-
-const SIZE = 1_000_001
-
-func getPrimes() []int {
-	isPrime := make([]bool, SIZE)
-	for i := 0; i < SIZE; i++ {
-		isPrime[i] = true
-	}
-	for i := 2; i < int(math.Sqrt(SIZE))+1; i++ {
-		if isPrime[i] {
-			for j := i * 2; j < SIZE; j += i {
-				isPrime[j] = false
-			}
-		}
-	}
-	res := make([]int, 0)
-	for i := 2; i < SIZE; i++ {
-		if isPrime[i] {
-			res = append(res, i)
-		}
-	}
-	return res
-}
 
 func main() {
 	bs.Split(bufio.ScanWords)
 	defer bw.Flush()
 
-	primes := getPrimes()
-	N := intInput()
-	cnt := 0
+	num := intInput()
+	var cnt uint
 
-	for _, p := range primes {
-		for N%p == 0 {
-			N /= p
+	for i := 2; i <= num; i++ {
+		for num%i == 0 {
+			num /= i
 			cnt++
 		}
-		if N == 1 {
+		if num == 1 {
 			break
 		}
 	}
-	fmt.Println(math.Ceil((math.Log2(float64(cnt)))))
+
+	fmt.Fprintln(bw, bits.Len(cnt-1))
 }
 
 var (
