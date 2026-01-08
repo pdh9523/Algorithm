e14cc05f3a21c14cfea0bfaffd96c3900f5120f4
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-01-08 14:28:26 +0900

    [Gold V] Title: チーズ (Cheese), Time: 388 ms, Memory: 141928 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/5558. チーズ （Cheese）/チーズ （Cheese）.py
@@ -0,0 +1,49 @@
+import sys; input = lambda: sys.stdin.readline().rstrip()
+from collections import deque
+
+
+dr = (1,0),(0,1),(-1,0),(0,-1)
+
+def findAll():
+    data = dict()
+
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j].isnumeric():
+                data[int(arr[i][j])] = (i,j)
+            elif arr[i][j] == "S":
+                data[0] = (i,j)
+    return data
+
+def bfs(now=0):
+    if now == K: return 0
+
+    sx,sy = position[now]
+    ex,ey = position[now+1]
+
+    q = deque([(sx,sy,0)])
+
+    while q:
+        x,y,cnt = q.popleft()
+
+        if (x,y) == (ex,ey): break
+
+        for dx,dy in dr:
+            nx,ny = x+dx, y+dy
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if arr[nx][ny] == "X": continue
+                if visit[nx][ny] == now: continue
+
+                visit[nx][ny] = now
+                q.append((nx,ny,cnt+1))
+    
+    return cnt + bfs(now+1)
+
+
+N,M,K = map(int,input().split())
+arr = [input() for _ in range(N)]
+position = findAll()
+
+visit = [[-1]*M for _ in range(N)]
+print(bfs())
