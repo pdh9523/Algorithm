0242cce238d0489ea87230c68e7642668f0bc908
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-01-21 12:30:19 +0900

    [Platinum V] Title: 보드 뒤집기 게임, Time: 212 ms, Memory: 33976 KB -BaekjoonHub

--- Python/백준/Platinum/25201. 보드 뒤집기 게임/보드 뒤집기 게임.py
+++ Python/백준/Platinum/25201. 보드 뒤집기 게임/보드 뒤집기 게임.py
@@ -4,22 +4,15 @@ SIZE = 10**5
 
 N,M = map(int,input().split())
 
-init_row = [0] * (SIZE+1)
-init_col = [0] * (SIZE+1)
-for _ in range(N):
+row = [0] * (SIZE+1)
+col = [0] * (SIZE+1)
+for _ in range(N+M):
     a,b = map(int,input().split())
-    init_row[a] += 1
-    init_col[b] += 1
-
-target_row = [0] * (SIZE+1)
-target_col = [0] * (SIZE+1)
-for _ in range(M):
-    a,b = map(int,input().split())
-    target_row[a] += 1
-    target_col[b] += 1
+    row[a] ^= 1
+    col[b] ^= 1
 
 for i in range(1,SIZE+1):
-    if init_row[i] % 2 != target_row[i] % 2 or init_col[i] % 2 != target_col[i] % 2:
+    if row[i] or col[i]:
         print("NO")
         break
 else: print("YES")
