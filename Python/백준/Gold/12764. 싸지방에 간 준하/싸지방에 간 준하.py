549d10d49c76a0592bced59f47d920158c9d9e56
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-01-30 20:32:45 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 396 ms, Memory: 61088 KB -BaekjoonHub

--- Python/백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
+++ Python/백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
@@ -6,21 +6,19 @@ arr = sorted([*map(int,input().split())] for _ in range(N))
 
 hq = []
 available = []
-count = [0]
-max_v = 0
+cnt = []
 for s,e in arr:
     while hq and hq[0][0] < s:
         _, now = heappop(hq)
         heappush(available, now)
 
     if not available:
-        max_v += 1
-        count.append(1)
-        heappush(hq, (e, max_v))
+        cnt.append(1)
+        heappush(hq, (e, len(cnt)))
     else:
         now = heappop(available)
-        count[now] += 1
+        cnt[now-1] += 1
         heappush(hq, (e, now))
 
-print(max_v)
-print(*count[1:])
+print(len(cnt))
+print(*cnt)
