c438c20a1be2d6432fd5d714f381d13947460b5f
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-02-03 10:53:17 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 36 ms, Memory: 11448 KB -BaekjoonHub

--- Go/백준/Gold/27172. 수 나누기 게임/수 나누기 게임.go
+++ Go/백준/Gold/27172. 수 나누기 게임/수 나누기 게임.go
@@ -6,23 +6,7 @@ import (
 	"strconv"
 )
 
-func max(args ...int) int {
-	res := 0
-	for _, arg := range args {
-		if res < arg {
-			res = arg
-		}
-	}
-	return res
-}
-
-func getSet(args ...int) map[int]bool {
-	res := make(map[int]bool)
-	for _, arg := range args {
-		res[arg] = true
-	}
-	return res
-}
+const SIZE = 1000000
 
 func main() {
 	bs.Split(bufio.ScanWords)
@@ -30,24 +14,31 @@ func main() {
 
 	N := intInput()
 	arr := make([]int, N)
+	check := make([]bool, SIZE+1)
+	ans := make([]int, SIZE+1)
+
 	for i := 0; i < N; i++ {
 		arr[i] = intInput()
+		check[arr[i]] = true
 	}
-	s := getSet(arr...)
-	maxValue := max(arr...)
 
-	ans := make([]int, maxValue+1)
-
-	for _, a := range arr {
-		for x := 2 * a; x < maxValue+1; x += a {
-			if _, exists := s[x]; exists {
-				ans[a]++
-				ans[x]--
+	for i := 2; i*2 <= SIZE; i++ {
+		if !check[i] {
+			continue
+		}
+		for j := 2; j*i <= SIZE; j++ {
+			if check[j*i] {
+				ans[i]++
+				ans[j*i]--
 			}
 		}
 	}
 
+	ans[1] = N
 	for _, a := range arr {
+		if check[1] {
+			ans[a]--
+		}
 		bw.WriteString(strconv.Itoa(ans[a]) + " ")
 	}
 }
