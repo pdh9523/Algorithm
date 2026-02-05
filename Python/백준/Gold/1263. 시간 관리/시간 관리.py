5f89fb2eb236d983e5cf33ebeed7f2d71e82073d
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-02-05 18:59:27 +0900

    [Gold V] Title: 시간 관리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1263. 시간 관리/시간 관리.py
+++ Python/백준/Gold/1263. 시간 관리/시간 관리.py
@@ -3,8 +3,8 @@ import sys; input = sys.stdin.readline
 N = int(input())
 arr = sorted([[*map(int,input().split())] for _ in range(N)], key=lambda x: -x[1])
 
-now = float('inf')
+now = 1000000
 for t,e in arr:
-    now = min(now-t, e-t)
+    now = min(now, e)-t
 
 print(now if now > 0 else -1)
