3f52a200d2abbb8720e323075817799f9ff07653
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-02-06 11:33:00 +0900

    [Silver V] Title: 중복된 숫자, Time: 252 ms, Memory: 2000 KB -BaekjoonHub

--- Go/백준/Silver/15719. 중복된 숫자/중복된 숫자.go
+++ Go/백준/Silver/15719. 중복된 숫자/중복된 숫자.go
@@ -4,18 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
-	bs.Buffer(buf, 100000000)
-	bs.Split(bufio.ScanWords)
 	defer bw.Flush()
 
 	N := intInput()
 	var sum int64 = 0
 
-	for i := 0; i < int(N); i++ {
+	for i := 0; i < N; i++ {
 		sum += int64(intInput())
 	}
 
@@ -24,17 +21,25 @@ func main() {
 }
 
 var (
-	bs  = bufio.NewScanner(os.Stdin)
-	bw  = bufio.NewWriter(os.Stdout)
-	buf = make([]byte, 100000000)
+	br = bufio.NewReaderSize(os.Stdin, 1<<20)
+	bw = bufio.NewWriter(os.Stdout)
 )
 
-func input() string {
-	bs.Scan()
-	return bs.Text()
-}
-
 func intInput() int {
-	num, _ := strconv.Atoi(input())
+	num := 0
+	for {
+		b, _ := br.ReadByte()
+		if b >= '0' && b <= '9' {
+			num = int(b - '0')
+			break
+		}
+	}
+	for {
+		b, err := br.ReadByte()
+		if err != nil || b < '0' || b > '9' {
+			break
+		}
+		num = num*10 + int(b-'0')
+	}
 	return num
 }
