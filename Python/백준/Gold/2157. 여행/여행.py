300c66c16a6ba7204f7e478abd5db56de0492f76
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-02-18 22:51:25 +0900

    [Gold IV] Title: 여행, Time: 1472 ms, Memory: 35124 KB -BaekjoonHub

--- Python/백준/Gold/2157. 여행/여행.py
+++ Python/백준/Gold/2157. 여행/여행.py
@@ -5,14 +5,14 @@ N,M,K = map(int,input().split())
 DP = [[-1]*M for _ in range(N+1)]
 DP[1][0] = 0
 
-graph = [[] for _ in range(N+1)]
+graph = [dict() for _ in range(N+1)]
 for i in range(K):
     a,b,c = map(int,input().split())
     if a > b: continue
-    graph[a].append((b,c))
+    graph[a][b] = max(graph[a].get(b, 0), c)
 
 for now in range(1,N+1):
-    for nxt, cost in graph[now]:
+    for nxt, cost in graph[now].items():
         for i in range(1,M):
             if DP[now][i-1] == -1: continue
             DP[nxt][i] = max(DP[nxt][i], DP[now][i-1] + cost)
