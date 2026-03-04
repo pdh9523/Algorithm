0802507a327e65267e6bb93e2efad48009d8f0ea
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-03-04 22:32:07 +0900

    [Silver V] Title: 반지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/5555. 반지/반지.py
+++ Python/백준/Silver/5555. 반지/반지.py
@@ -1,18 +1,9 @@
 import sys; input = lambda: sys.stdin.readline().rstrip()
 
-def check():
-    for i in range(len(ring)):
-        for j in range(len(word)):
-            if ring[(i+j)%len(ring)] != word[j]:
-                break
-        else:
-            return True
-    return False
-
 word = input()
 ans = 0
 for _ in range(int(input())):
-    ring = input()
-    ans += check()
+    ring = input() * 2
+    ans += word in ring
 
 print(ans)
