b1dd4dd0570abfda2cbb589d274327af33d3b65d
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-04-10 22:20:12 +0900

    [Bronze II] Title: 문제 셋 만들기, Time: 4 ms, Memory: 940 KB -BaekjoonHub

---
+++ Go/백준/Bronze/35493. 문제 셋 만들기/문제 셋 만들기.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+	bs.Split(bufio.ScanWords)
+	defer bw.Flush()
+
+	N := intInput()
+	if N == 1 && intInput()%2 == 1 {
+		bw.WriteString("NO")
+		return
+	}
+	bw.WriteString("YES")
+}
+
+var (
+	bs = bufio.NewScanner(os.Stdin)
+	bw = bufio.NewWriter(os.Stdout)
+)
+
+func input() string {
+	bs.Scan()
+	return bs.Text()
+}
+
+func intInput() int {
+	num, _ := strconv.Atoi(input())
+	return num
+}
