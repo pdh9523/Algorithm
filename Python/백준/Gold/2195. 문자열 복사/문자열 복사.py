8ed2258ebb39b4b5267a3b6c856eab7fd5b9daa2
Donghyeon Park <156387954+pdh9523@users.noreply.github.com>
2026-04-16 17:57:45 +0900

    [Gold V] Title: 문자열 복사, Time: 752 ms, Memory: 246252 KB -BaekjoonHub

--- Python/백준/Gold/2195. 문자열 복사/문자열 복사.py
+++ Python/백준/Gold/2195. 문자열 복사/문자열 복사.py
@@ -1,11 +1,19 @@
+data = dict()
+
 s = input()
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        tmp = s[i:j]
+        data.setdefault(len(tmp), set()).add(tmp)
 
+t = input()
 ans = 0
-t = ""
-for c in input():
-    t += c
-    if t not in s:
-        t = c
-        ans += 1
+start = 0 
+while start < len(t):
+    for end in range(len(t), start, -1):
+        if t[start:end] in data.setdefault(end-start, set()):
+            start = end
+            ans += 1
+            break
 
-print(ans + int(bool(t)))
+print(ans)
